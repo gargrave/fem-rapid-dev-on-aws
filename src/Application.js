1b59dab30c6b1d7c9d5630dd5184b1b19b7f6a4b
Gabe Hargrave <gkhargrave@gmail.com>
2018-09-30 15:55:24 -0700

    Wire up all CRUD actions

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 
 import NewGrudge from './NewGrudge';
@@ -6,27 +7,56 @@ import Grudges from './Grudges';
 
 import './Application.css';
 
+const API_NAME = 'grudgesCRUD';
+const API_PATH = '/grudges';
+
 class Application extends Component {
   state = {
     grudges: [],
   };
 
-  addGrudge = grudge => {
-    this.setState({ grudges: [grudge, ...this.state.grudges] });
+  async componentDidMount() {
+    try {
+      const grudges = await API.get(API_NAME, API_PATH);
+      this.setState({ grudges });
+    } catch(err) {
+      console.error('There was an error fetching the grudges!');
+    }
+  }
+
+  addGrudge = async (grudge) => {
+    try {
+      await API.post(API_NAME, API_PATH, { body: grudge });
+      this.setState(({ grudges }) => ({ grudges: [grudge, ...grudges] }));
+    } catch(err) {
+      console.log('There was an error creating the new grudge!');
+    }
   };
 
-  removeGrudge = grudge => {
-    this.setState({
-      grudges: this.state.grudges.filter(other => other.id !== grudge.id),
-    });
+  removeGrudge = async (grudge) => {
+    try {
+      await API.del(API_NAME, `${API_PATH}/object/${grudge.id}`)
+      this.setState(({ grudges }) => ({
+        grudges: grudges.filter(other => other.id !== grudge.id),
+      }));
+    } catch(err) {
+      console.error('There was an error deleting the grudge!');
+    }
   };
 
   toggle = grudge => {
-    const othergrudges = this.state.grudges.filter(
-      other => other.id !== grudge.id,
-    );
     const updatedGrudge = { ...grudge, avenged: !grudge.avenged };
-    this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+    this.setState(({ grudges }) => { 
+      const othergrudges = grudges.filter(other => other.id !== grudge.id);
+      return { grudges: [updatedGrudge, ...othergrudges] };
+    }, async () => {
+      try {
+        await API.put(API_NAME, API_PATH, { body: updatedGrudge })
+      } catch(err) {
+        console.error('There was an error updating the grduge!');
+      }
+    });
+
   };
 
   render() {
