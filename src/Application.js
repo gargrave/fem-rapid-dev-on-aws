b5c0e6f69a7d37ce7481dbbb84ba17c767ff34bb
Gabe Hargrave <gkhargrave@gmail.com>
2018-10-07 17:58:46 -0700

    Update all actions to use GraphQL queries

--- src/Application.js
+++ src/Application.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 
-import { createGrudge, listGrudges } from './graphql'
+import { 
+  CreateGrudge, 
+  DeleteGrudge, 
+  ListGrudges, 
+  SubscribeToNewGrudge,
+  UpdateGrudge,
+} from './graphql'
 
 import NewGrudge from './NewGrudge';
 import Grudges from './Grudges';
@@ -18,8 +24,16 @@ class Application extends Component {
   };
 
   async componentDidMount() {
+    // NOTE: uncomment for real-time updates when Grudges are added
+    // API.graphql(graphqlOperation(SubscribeToNewGrudge)).subscribe({
+    //   next: response => {
+    //     const grudge = response.value.data.onCreateGrudge;
+    //     this.setState(({ grudges }) => ({ grudges: [...grudges, grudge] }))
+    //   }
+    // })
+
     try {
-      const response = await API.graphql(graphqlOperation(listGrudges))
+      const response = await API.graphql(graphqlOperation(ListGrudges))
       const grudges = response.data.listGrudges.items
       this.setState({ grudges });
     } catch(err) {
@@ -30,7 +44,7 @@ class Application extends Component {
   addGrudge = grudge => {
     this.setState(({ grudges }) => ({ grudges: [...grudges, grudge] }), async () => {
       try {
-        await API.graphql(graphqlOperation(createGrudge, grudge))
+        await API.graphql(graphqlOperation(CreateGrudge, grudge))
       } catch(err) {
         console.log('There was an error creating the new grudge!');
         this.setState(({ grudges }) => ({ 
@@ -45,7 +59,7 @@ class Application extends Component {
       grudges: grudges.filter(other => other.id !== grudge.id),
     }), async () => {
       try {
-        await API.del(API_NAME, `${API_PATH}/object/${grudge.id}`)
+        await API.graphql(graphqlOperation(DeleteGrudge, grudge))
       } catch(err) {
         console.error('There was an error deleting the grudge!');
         this.setState(({ grudges }) => ({ grudges: [...grudges, grudge] }))
@@ -60,7 +74,7 @@ class Application extends Component {
       return { grudges: [updatedGrudge, ...othergrudges] };
     }, async () => {
       try {
-        await API.put(API_NAME, API_PATH, { body: updatedGrudge })
+        await API.graphql(graphqlOperation(UpdateGrudge, updatedGrudge))
       } catch(err) {
         console.error('There was an error updating the grduge!');
         // return grudges to previous state on error
--- src/graphql.js
+++ src/graphql.js
@@ -1,4 +1,4 @@
-export const listGrudges = `
+export const ListGrudges = `
   query ListGrudges {
     listGrudges {
       items {
@@ -11,17 +11,57 @@ export const listGrudges = `
   }
 `
 
-export const createGrudge = `
+export const CreateGrudge = `
   mutation CreateGrudge(
     $person: String!
     $deed: String!
     $avenged: Boolean!
   ) {
     createGrudge(input: {
-      person: $person,
-      deed: $deed,
+      person: $person
+      deed: $deed
       avenged: $avenged
     }) {
+      id
+      person
+      deed
+      avenged
+    }
+  }
+`
+
+export const UpdateGrudge = `
+  mutation UpdateGrudge(
+    $id: ID!
+    $avenged: Boolean!
+  ) {
+    updateGrudge(input: {
+      id: $id
+      avenged: $avenged
+    }) {
+      id
+      person
+      deed
+      avenged
+    }
+  }
+`
+
+export const DeleteGrudge = `
+  mutation DeleteGrudge(
+    $id: ID!
+  ) {
+    deleteGrudge(input: {
+      id: $id
+    }) {
+      id
+    }
+  }
+`
+
+export const SubscribeToNewGrudge = `
+  subscription SubscribeToNewGrudge {
+    onCreateGrudge {
       id
       person
       deed
